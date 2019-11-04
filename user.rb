fb2f02ed64820a03b2875983d0e154c67e63b8ef
ruslan <sqwer1986@gmail.com>
2019-11-04 18:04:00 +0300

    step 1

---
+++ user.rb
@@ -0,0 +1,61 @@
+
+class User
+  attr_accessor :bul
+
+  @@arr = %w[a b c d e f g h i]
+
+  def self.arr
+    @@arr
+  end
+
+  def initialize
+    @letter = nil
+    @arr_letter = []
+    @bul = false
+  end
+
+  def inspecting(letter)
+    @letter = letter
+    unless @@arr.include?(@letter)
+      while @@arr.include?(@letter) == false
+        puts 'Hes not the index in the feild'
+        puts "Enter index the feild"
+        @letter = STDIN.gets.strip
+      end
+    end
+
+    @@arr.delete(@letter)
+
+    @arr_letter << @letter
+
+    show
+
+    if @arr_letter.include?('a') && @arr_letter.include?('b') && @arr_letter.include?('c')
+      result
+
+      result elsif @arr_letter.include?('d') && @arr_letter.include?('e') && @arr_letter.include?('f')
+
+      result elsif @arr_letter.include?('g') && @arr_letter.include?('h') && @arr_letter.include?('i')
+
+      result elsif @arr_letter.include?('a') && @arr_letter.include?('d') && @arr_letter.include?('g')
+
+      result elsif @arr_letter.include?('b') && @arr_letter.include?('e') && @arr_letter.include?('h')
+
+      result elsif @arr_letter.include?('c') && @arr_letter.include?('f') && @arr_letter.include?('i')
+
+      result elsif @arr_letter.include?('a') && @arr_letter.include?('e') && @arr_letter.include?('i')
+
+      result elsif @arr_letter.include?('c') && @arr_letter.include?('e') && @arr_letter.include?('g')
+    end
+  end
+
+  def result
+    @bul = true
+  end
+
+  def show
+    puts "Выбранные поля #{@arr_letter}"
+  end
+end
+
+
---
+++ x_o.rb
@@ -0,0 +1,37 @@
+require_relative 'user.rb'
+
+puts "start gema"
+
+user1 = User.new
+
+user2 = User.new
+
+while User.arr.size > 0
+  File.open('file1.txt', 'r'){|f| puts f.readlines}
+
+  puts "Enter index the feild"
+  letter_user = STDIN.gets.strip
+
+  user1.inspecting(letter_user)
+
+  sleep 0.5
+
+  if User.arr.size > 0
+    bot_rand_namder = rand(User.arr.size)
+    letter_bot = User.arr[bot_rand_namder]
+    user2.inspecting(letter_bot)
+  end
+
+  if user1.bul == true && user2.bul == true
+    puts 'Боевая ничья'
+    exit
+  elsif user1.bul == true
+    puts 'Поздравляю Вы победили!!!'
+    exit
+  elsif user2.bul == true
+    puts 'Компьютер победил'
+    exit
+  end
+end
+
+puts 'Боевая ничья'
